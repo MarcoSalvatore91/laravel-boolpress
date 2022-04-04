421652d8cea4b3a00fbb2a5ffdc313f6e73f0f1e
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-04 23:26:04 +0200

    created new components navpage

---
+++ resources/js/components/NavPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+        <ul class="pagination">
+            <li @click="$emit('page-change', pagination.currentPage - 1)" role="button" class="page-item" v-if="pagination.currentPage > 1">
+                <span class="page-link">Previous</span>
+            </li>
+            <li @click="$emit('page-change', page)" :class="{ active:page === pagination.currentPage }" role="button" v-for="page in pagination.lastPage" :key="page" class="page-item">
+                <span class="page-link">{{ page }}</span>
+            </li>
+            <li @click="$emit('page-change', pagination.currentPage + 1)" role="button" class="page-item" v-if="pagination.lastPage > pagination.currentPage">
+                <span class="page-link">Next</span>
+            </li>
+        </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+name: 'NavPage',
+props: ["pagination"]
+}
+</script>
+
+<style>
+
+</style>
