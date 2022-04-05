bb65f50cac62004554dfa2ab168ae5e3028c0c90
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 16:24:38 +0200

    added button back in NotFoundPgae

--- resources/js/components/pages/NotFoundPage.vue
+++ resources/js/components/pages/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="NotFoundPage" class="text-center mb-5">
-        <h1>404 || PAGINA NON TROVATA</h1>
+    <div id="NotFoundPage" class="text-center mt-5">
+        <h1 class="text-muted">404 || PAGINA NON TROVATA</h1>
+        <button class="btn btn-primary" @click="$router.back()">Indietro</button>
     </div>
 </template>
 
