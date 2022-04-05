09b5f573661028a028d873b1d4603754b851749a
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 17:52:02 +0200

    created new components SinglePost

---
+++ resources/js/components/pages/SinglePost.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="single-post">
+      <PostCard />
+  </section>
+</template>
+
+<script>
+import PostCard from '../posts/PostCard.vue';
+
+export default {
+name: "SinglePost",
+components: {
+    PostCard,
+}
+}
+</script>
+
+<style>
+
+</style>
