bba72f016a042d6ee904a7abc1f5c897647f8334
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 16:08:20 +0200

    import all components in routes.js

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,3 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
+
+import Home from './components/pages/Home.vue'
+import Contacts from './components/pages/Contacts.vue'
+import NotFoundPage from './components/pages/NotFoundPage.vue'
