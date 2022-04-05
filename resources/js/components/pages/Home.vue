53a8f9162dc5ae8068bf4385813a141c31326d5b
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 16:02:37 +0200

    import PostsList

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <section id="home">
-
+      <PostsList />
   </section>
 </template>
 
 <script>
+import PostsList from '../posts/PostsList.vue'
+
 export default {
 name: "Home",
+components: {
+    PostsList,
+}
 }
 </script>
 
