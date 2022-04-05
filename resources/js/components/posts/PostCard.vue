cedb8abce3abacc4e292972423f3f2ea553c2a7c
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 17:52:27 +0200

    created button for SinglePost in Postcard

--- resources/js/components/posts/PostCard.vue
+++ resources/js/components/posts/PostCard.vue
@@ -1,9 +1,17 @@
 <template>
 <div id="post-card" class="row justify-content-center">
     <div class="card col-12 my-3" style="width: 18rem;" v-for="post in posts" :key="post.id">
-        <div class="card-body">
-            <h2 class="card-title">{{ post.title }}</h2>
-            <p class="card-text">{{ post.content }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+
+            <div class="card-body">
+                <h2 class="card-title">{{ post.title }}</h2>
+                <p class="card-text">{{ post.content }}</p>
+            </div>
+
+            <div>
+                <router-link class="btn btn-primary" :to="{ name: 'single-post', params: { id: post.id } }">Info</router-link>
+            </div>
+
         </div>
         <ul class="list-group list-group-flush">
             <li class="list-group-item"><strong>Creato il: </strong>{{ getDate(post.created_at) }}</li>
