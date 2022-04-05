2cf4a835f2ad8fa90bba012a7b27c48900dc214f
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 16:30:38 +0200

    changed header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="head">
-      <h1 class="p-3">{{ title }}</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">{{ title }}</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'contacts' }">Features</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
