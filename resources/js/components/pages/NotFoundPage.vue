0fd15ab09bab05d8d10cdaaf5d2b11033274a173
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-05 16:08:35 +0200

    created new components NotFoundPage

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="NotFoundPage" class="text-center mb-5">
+        <h1>404 || PAGINA NON TROVATA</h1>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'NotFoundPage'
+}
+</script>
+
+<style>
+
+</style>
