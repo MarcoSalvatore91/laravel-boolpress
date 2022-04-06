05a380956a5e4d3037e4a4b31b873cc115cbf2bc
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-06 18:04:39 +0200

    created function for request

--- app/Http/Controllers/Api/ContactMessageController.php
+++ app/Http/Controllers/Api/ContactMessageController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMessageMail;
 
 class ContactMessageController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        $data = $request->all();
+
+
+
+        $mail = new ContactMessageMail($data);
+        Mail::to(env('MAIL_CONTACT_ADDRESS'))->send($mail);
+
+        return response('Mail Sent', 204);
+    }
 }
