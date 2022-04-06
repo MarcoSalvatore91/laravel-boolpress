c1009046b5a1c2a0047ea8d16c560ba78d7b9035
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-04-06 16:54:00 +0200

    insert $contact in return 'mail.messages'

--- app/Mail/ContactMessageMail.php
+++ app/Mail/ContactMessageMail.php
@@ -30,6 +30,6 @@ class ContactMessageMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('mail.messages');
+        return $this->markdown('mail.messages')->with(['contact' => $this->contact]);
     }
 }
